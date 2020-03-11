0696ac82506f1b09e902350484bc51c811286bff
Altaf Quadri <altafmquadri@gmail.com>
2020-03-11 15:15:01 -0400

    hooks practice

---
+++ src/SimpleFormInputHook.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import useInputState from './hooks/useInputState'
+
+const SimpleFormInputHook = () => {
+    const [email, updateEmail, resetEmail] = useInputState('')
+
+    return (
+        <div>
+            <h1>The value is...{email}</h1>
+            <input
+                type='text'
+                value={email}
+                onChange={updateEmail} />
+            <button onClick={resetEmail}>Submit</button>
+        </div>
+    );
+}
+
+export default SimpleFormInputHook;
---
+++ src/Toggler.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import useToggle from './hooks/useToggle'
+
+
+const Toggler = () => {
+    const [isHappy, toggleIsHappy] = useToggle(true)
+    const [isHeartBroken, toggleIsHeartBroken] = useToggle(false)
+
+    return (
+        <div>
+            <h1 onClick={toggleIsHappy}>{isHappy ? '😊' : '😢'}</h1>
+            <h1 onClick={toggleIsHeartBroken}>{isHeartBroken ? '💔' : '❤'}</h1>
+        </div>
+    );
+}
+
+export default Toggler;
+
+
+
